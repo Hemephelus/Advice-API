515e7a6e5595af7f563f6e4a70654e17483d1cda
Nwachukwu Ujubuonu <nwachukwuujubuonu@gmail.com>
2022-06-28 23:34:26 +0100

    Styled and linked the Javascript

--- advice.js
+++ advice.js
@@ -1,11 +1,17 @@
-const adviceslip =  'https://api.adviceslip.com/advice'
 
+const diceButton = document.querySelector('button')
+const adviceId = document.querySelector('#adviceId')
+const mainAdvice = document.querySelector('#mainAdvice')
 
-async function getText(file) {
-    let adviceApi = await fetch(file);
+diceButton.onclick = getText
+
+async function getText() {
+    const adviceslip =  'https://api.adviceslip.com/advice'
+    let adviceApi = await fetch(adviceslip);
     let AdviceApiText = await adviceApi.text();
    let getJsonFormat = JSON.parse(AdviceApiText)
-    let adviceId = getJsonFormat.slip.id
-    let mainAdvice = getJsonFormat.slip.advice
+ adviceId.textContent = getJsonFormat.slip.id
+ mainAdvice.textContent = getJsonFormat.slip.advice
   }
 
+ 
